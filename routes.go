5475a4b2641d61f17467cbeeeb29f0a6dda17a2d
Declan Healey <declanhealey@C02D8PQ0ML7H.permutive.com>
2022-06-26 20:11:55 +0100

    Added request authorization middleware

--- routes.go
+++ routes.go
@@ -1,10 +1,34 @@
 package main
 
+import (
+	"net/http"
+	"github.com/gin-gonic/gin"
+	"github.com/gin-contrib/sessions"
+)
+
+// Middleware for authorizing request
+func authorizeRequest() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		session := sessions.Default(c)
+		v := session.Get("user-id")
+		if v == nil {
+			c.IndentedJSON(http.StatusUnauthorized, nil)
+			c.Abort()
+		}
+		c.Next()
+	}
+}
+
 func initializeRoutes(c Connection) {
 	// Handle the index route
+	authorized := router.Group("/api/user")
+	authorized.Use(authorizeRequest()) 
+	{
+		authorized.POST("/project", c.postProject)
+	}
 	router.GET("/", hello)
-	router.POST("/api/project", c.postProject)
 	router.GET("/api/project/:name", c.getProject)
+	// router.POST("/api/project/", c.postProject)
 	router.GET("/login", loginHandler)
 	router.GET("/auth", c.authHandler)
 }
