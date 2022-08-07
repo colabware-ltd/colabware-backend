ce08b2fa7d97e2fe2d624e55491e219612f38214
Declan Healey <declanhealey@C02D8PQ0ML7H.permutive.com>
2022-08-07 18:46:47 -0400

    Project and token contract enhancements

--- contracts/Project.sol
+++ contracts/Project.sol
@@ -9,18 +9,27 @@ import './ERC20PresetMinterPauser.sol';
  * @dev Controls project permission management
  */
 contract Project {
-    address[] public maintainers;
+    address[] private maintainers;
     address private wallet;
-    ERC20 private token;
+    ERC20PresetMinterPauser private token;
     
     // TODO: Use to ensure that project wallet must always maintain a minimum of the
-    // reserved tokens specified.
+    // reserved tokens specified (i.e. when selling)
     uint256 private reservedTokens;
     
     // TODO: Add address of maintainer creating project.
-    constructor(string memory _tokenName, string memory _tokenSymbol, uint256 _totalTokens, uint256 _reservedTokens) {
+    constructor(string memory _tokenName, string memory _tokenSymbol, uint256 _totalTokens, uint256 _reservedTokens, address _wallet) {
         token = new ERC20PresetMinterPauser(_tokenName, _tokenSymbol);
+        token.mint(_wallet, _totalTokens);
         reservedTokens = _reservedTokens;
+        wallet = _wallet;
     }
 
+    function getMaintainerTokens() public view returns (uint256) {
+        return reservedTokens;
+    }
+
+    function getTokenSupply() public view returns (uint256) {
+        return token.totalSupply();
+    }
 }
